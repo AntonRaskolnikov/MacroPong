23338b92b3dddbcd30a5371f575e730f3a1bf25b
Anton Raskolnikov <theunknown1001002@gmail.com>
2024-12-30 22:17:37 +0700

    dlfhwadklfhawkhf

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use macroquad::prelude::*;
 
 struct Player {
+    x: f32,
     y: f32,
     width: f32,
-    height: f32
+    height: f32,
+    collision: Vec<(f32, f32)>
 }
 
 impl Player {
@@ -15,6 +17,28 @@ impl Player {
             self.y += speed;
         }
         self.y = self.y.clamp(0.0, 500.0);
+
+        let mut collision: Vec<(f32, f32)> = Vec::new();
+
+        for point_x in 0..self.width as i32 {
+            for point_y in 0..self.height as i32 {
+                collision.push((self.x + point_x as f32, self.y + point_y as f32));
+            }
+        }
+
+        self.collision = collision;
+
+    }
+    fn from(x: f32, y: f32, width: f32, height: f32) -> Self {
+        let mut collision: Vec<(f32, f32)> = Vec::new();
+
+        for point_x in 0..width as i32 {
+            for point_y in 0..height as i32 {
+                collision.push((x + point_x as f32, y + point_y as f32));
+            }
+        }
+
+        Player { x: x, y: y, width: width, height: height, collision: collision }
     }
 }
 
@@ -31,8 +55,12 @@ struct Ball {
 }
 
 impl Ball {
-    fn update(&self) {
-        
+    fn update(&mut self, player: &Player) {
+        self.x += self.velocity.x;
+        self.y += self.velocity.y;
+        for i in 0..player.collision.len() {
+
+        }
     }
 }
 
@@ -43,11 +71,7 @@ async fn main() {
 
     pub const SPEED: f32 = 3.0;
 
-    let mut player = Player {
-        y: 100.,
-        width: 30.,
-        height: 90.
-    };
+    let mut player = Player::from(100., 100., 30., 90.);
     let mut ball = Ball{ 
         x: 400.0, 
         y: 310.0,
@@ -61,8 +85,8 @@ async fn main() {
         draw_rectangle(100.0, player.y, player.width, player.height, WHITE);
         player.update(SPEED);
 
-        draw_circle(ball.x, ball.y, 10.0, WHITE);
-        ball.update();
+        draw_rectangle(ball.x, ball.y, 20., 20., WHITE);
+        ball.update(&player);
         
 
         next_frame().await;
